94ae5c59f57d8c22b1f18ff0e2b42ead30e16de1
Mali Michael <malimccalla@gmail.com>
2017-12-26 12:04:05 +0000

    User and playlists resolvers

--- src/resolvers/playlist.js
+++ src/resolvers/playlist.js
@@ -1,2 +1,16 @@
 // @flow
-export default {};
+import Playlist from '../database/Playlist';
+import User from '../database/User';
+
+export default {
+  Query: {
+    allPlaylists: (parent: *, args: *, context: *, info: *) =>
+      Playlist.findAll(),
+    getPlaylist: (parent: *, { id }: *, context: *, info: *) =>
+      Playlist.findById(id),
+  },
+  Playlist: {
+    artist: ({ userId }: *, args: *, context: *, info: *) =>
+      User.findById(userId),
+  },
+};
--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,2 +1,14 @@
 // @flow
-export default {};
+import User from '../database/User';
+import Playlist from '../database/Playlist';
+
+export default {
+  Query: {
+    allUsers: (parent: *, args: *, context: *, info: *) => User.findAll(),
+    getUser: (parent: *, { id }: *, context: *, info: *) => User.findById(id),
+  },
+  User: {
+    playlists: (parent: *, args: *, context: *, info: *) =>
+      Playlist.findAllByUserId(parent.id),
+  },
+};
