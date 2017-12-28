11e63776202d794d2fa35e57032c051db34a2ecf
Mali Michael <malimccalla@gmail.com>
2017-12-28 12:50:02 +0000

    cors

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { express as voyager } from 'graphql-voyager/middleware';
+import cors from 'cors';
 
 import schema from './src/schema';
 
 const PORT = 5000;
 const app = express();
 const graphqlEndpoint = '/graphql';
+
+app.use(cors()); // TODO
 
 // bodyParser is needed just for POST.
 app.use(
