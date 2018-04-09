929792860a2210bce30d4636975c3ba26a61b97d
Mali Michael <malimccalla@gmail.com>
2018-04-09 18:30:47 +0100

    Add reminder to swap out current schema merging

--- src/schema.ts
+++ src/schema.ts
@@ -3,8 +3,9 @@ import path from 'path';
 import { makeExecutableSchema } from 'graphql-tools';
 import { fileLoader, mergeResolvers, mergeTypes } from 'merge-graphql-schemas';
 
+// NOTE: since apollo 2.0 we can use graphql-tools to do this
+// TODO: Swap out 'merge-graphql-schemas' and use 'graphql-tools' instead
 const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './types')));
-
 const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')));
 
 const schema = {
